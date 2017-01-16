0360cbc001eb119a83c3e5001a85d78634d6d8a9
oliver <oliver@olivers-MacBook-Pro.local>
2017-01-16 12:48:15 +0800

    afnetworking request

--- Parking/ViewController.m
+++ Parking/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"//主要用于网络请求方法
+
 
 @interface ViewController ()
 
@@ -17,12 +19,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self obtainData];
+    
+   
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(void)obtainData
+{
+    // 启动系统风火轮
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    NSString *URLString = @"http://10.10.2.44:3000";
+
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager GET:URLString parameters:NULL progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        NSLog(@"JSON: %@", responseObject);
+//        NSDictionary *dic=[NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableLeaves error:nil];
+        //接下来一步一步解析。知道得到你想要的东西。
+        NSDictionary* arrayDic =[responseObject objectAtIndex:0];
+        NSArray* resultArray = [arrayDic objectForKey:@"data"];
+        
+    
+        
+        NSArray* resultData = [NSString stringWithFormat:@"%@",[resultArray objectAtIndex:0]];
+        
+        NSLog(@"Data: %@", [responseObject[0] objectForKey:@"data"] [0][0]);
+        
+        
+        
+
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        NSLog(@"JSON: %@", error);
+
+    }];
+    
+    
 }
 
 
